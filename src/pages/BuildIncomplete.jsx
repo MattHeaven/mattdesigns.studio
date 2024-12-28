3b6908b92d3aad36384df93e2d3a43064cc4cb34
Matt Heaven <imattheaven@gmail.com>
2024-12-28 19:08:18 +0300

    Build Incomplete

---
+++ src/pages/BuildIncomplete.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function BuildIncomplete() {
+  return (
+    <div className='p-4 flex items-center justify-center'>
+        <p>This website is still being developed!</p>
+    </div>
+  )
+}
+
+export default BuildIncomplete
